b930728626a95e8f2c656c89e0b4631d791b9914
astromanreal <astroman@gmail.com>
2026-01-07 20:07:24 +0530

    Mission tracking feature added

--- src/routes/missions.js
+++ src/routes/missions.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { getMissions, getMission } from '../controllers/missions.js';
+import {
+  getMissions,
+  getMission,
+  getMissionTrackers,
+  trackMission
+} from '../controllers/missions.js';
 
 const router = express.Router();
 
 router.route('/').get(getMissions);
 router.route('/:id').get(getMission);
+router.route('/:id/trackedby').get(getMissionTrackers);
+router.route('/track/:missionId').get(trackMission);
 
 export default router;
