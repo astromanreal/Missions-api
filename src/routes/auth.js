5b0c00a8bfb18bb99458f6345c24cfed5bf65205
astromanreal <astroman@gmail.com>
2026-01-08 13:21:15 +0530

    User profile is not private from now

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,27 @@
-import express from 'express';
-import { register, login, getMe, verifyOTP, getUser, logout, forgotPassword, resetPassword, updateMe } from '../controllers/authController.js';
-import { protect } from '../middleware/auth.js';
+import express from "express";
+import {
+  register,
+  login,
+  getMe,
+  verifyOTP,
+  getUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  updateMe,
+} from "../controllers/authController.js";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/verify-otp', verifyOTP);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
-router.get('/me', protect, getMe);
-router.put('/me', protect, updateMe);
-router.get('/user/:id', protect, getUser);
-router.post('/logout', protect, logout);
+router.post("/register", register);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/me", protect, getMe);
+router.put("/me", protect, updateMe);
+router.get("/user/:id", getUser);
+router.post("/logout", protect, logout);
 
 export default router;
