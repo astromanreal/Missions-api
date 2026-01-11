2c84ccdd72bc9e3a991a9473e4b9d446fb552d97
astromanreal <astroman@gmail.com>
2026-01-11 13:05:49 +0530

    now user can update and delete mission updates

--- src/controllers/updates.js
+++ src/controllers/updates.js
@@ -51,3 +51,60 @@ export const getMissionUpdates = asyncHandler(async (req, res, next) => {
     data: updates,
   });
 });
+
+/**
+ * @desc    Update a mission update
+ * @route   PUT /api/v1/updates/:id
+ * @access  Private
+ */
+export const updateMissionUpdate = asyncHandler(async (req, res, next) => {
+  let update = await MissionUpdate.findById(req.params.id);
+
+  if (!update) {
+    return next(new ErrorResponse(`Mission update not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is the author
+  if (update.author.toString() !== req.user.id) {
+    return next(new ErrorResponse(`User not authorized to update this mission update`, 401));
+  }
+
+  // Make sure update is pending
+  if (update.status !== 'pending') {
+      return next(new ErrorResponse(`Cannot edit an update that has already been reviewed`, 400));
+  }
+
+  update = await MissionUpdate.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ success: true, data: update });
+});
+
+/**
+ * @desc    Delete a mission update
+ * @route   DELETE /api/v1/updates/:id
+ * @access  Private
+ */
+export const deleteMissionUpdate = asyncHandler(async (req, res, next) => {
+  const update = await MissionUpdate.findById(req.params.id);
+
+  if (!update) {
+    return next(new ErrorResponse(`Mission update not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is the author
+  if (update.author.toString() !== req.user.id) {
+    return next(new ErrorResponse(`User not authorized to delete this mission update`, 401));
+  }
+
+  // Make sure update is pending
+  if (update.status !== 'pending') {
+      return next(new ErrorResponse(`Cannot delete an update that has already been reviewed`, 400));
+  }
+
+  await update.remove();
+
+  res.status(200).json({ success: true, data: {} });
+});
--- src/routes/updates.js
+++ src/routes/updates.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import {
+  addMissionUpdate,
+  getMissionUpdates,
+  updateMissionUpdate,
+  deleteMissionUpdate,
+} from '../controllers/updates.js';
 import { protect } from '../middleware/auth.js';
 
 const router = express.Router({ mergeParams: true });
 
-// Placeholder for controllers - we will create these next
-import { addMissionUpdate, getMissionUpdates } from '../controllers/updates.js'; 
+router
+  .route('/')
+  .post(protect, addMissionUpdate)
+  .get(getMissionUpdates);
 
-router.route('/').post(protect, addMissionUpdate);
-router.route('/').get(getMissionUpdates);
+router
+  .route('/:id')
+  .put(protect, updateMissionUpdate)
+  .delete(protect, deleteMissionUpdate);
 
 export default router;
